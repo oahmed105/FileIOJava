c50d70db2cec5c913a2208e979e608b8c432cdfa
oahmed105 <oahmed10598@gmail.com>
2023-07-07 19:35:45 -0400

    finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -12,20 +12,18 @@ public class ReadFour {
     public static void main(String[] args) {
         try {
             Scanner fileIn = new Scanner(new File("/Users/osama/LocalProjects/FileIOJava/Step4/text.txt"));
-
+            int sum = 0;
             while (fileIn.hasNext())
             {
                 // Reads the entire line as a string
                 String lineIn = fileIn.nextLine();
                 // Split the line into a String array
-                String[] input = lineIn.split(" ");
-                int[] nums = new int[input.length];
-                int sum = 0;
-                for (int i = 0; i < nums.length; i ++) {
-                    sum += Integer.parseInt(input[i]);
-                }
-                System.out.println(sum);
+                //String[] input = lineIn.split(" ");
+                //int[] num = new int[lineIn];
+
+                sum += Integer.parseInt(lineIn);
             }
+            System.out.println(sum);
         }
         catch (IOException e) {
             System.out.println("File not found");
